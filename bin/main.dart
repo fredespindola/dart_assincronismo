a8d2cf4b77f22895a8217271cb8beac1267183f0
Frederico Espindola <fredgomes10@gmail.com>
2025-10-16 12:44:18 -0300

    completo o sendDataAsync

--- bin/main.dart
+++ bin/main.dart
@@ -1,25 +1,46 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
+
 void main() {
-  requestData();
+  // requestData();
+  //requestDataAsync();
+  sendDataAsync({
+    "id": "NEW001",
+    "name": "Flutter",
+    "lastName": "Dart",
+    "balance": 5000,
+  });
 }
 
 void requestData() {
-    String url = "https://gist.githubusercontent.com/ricarthlima/a0eb198cb7a70696c4031e7e577de0cd/raw/356ce2c39dfd58d3d2e948d1ad87ea828544f1db/accounts.json";
-    Future<Response> futureResponse = get(Uri.parse(url));
-    print(futureResponse);
-    futureResponse.then(
-        (Response response) {
-            print(response);
-            print(response.body);
-            List<dynamic> listAccounts = json.decode(response.body);
-            Map<String, dynamic> mapCarla = listAccounts.firstWhere(
-                (element) => element["name"] == "Carla",
-            );
-            print(mapCarla["balance"]);
-        },
+  String url =
+      "https://gist.githubusercontent.com/ricarthlima/a0eb198cb7a70696c4031e7e577de0cd/raw/356ce2c39dfd58d3d2e948d1ad87ea828544f1db/accounts.json";
+  Future<Response> futureResponse = get(Uri.parse(url));
+  print(futureResponse);
+  futureResponse.then((Response response) {
+    print(response);
+    print(response.body);
+    List<dynamic> listAccounts = json.decode(response.body);
+    Map<String, dynamic> mapCarla = listAccounts.firstWhere(
+      (element) => element["name"] == "Carla",
     );
-    
-    print("Última coisa a acontecer na função.");
+    print(mapCarla["balance"]);
+  });
+
+  print("Última coisa a acontecer na função.");
+}
+
+Future<List<dynamic>> requestDataAsync() async {
+  String url =
+      "https://gist.githubusercontent.com/ricarthlima/a0eb198cb7a70696c4031e7e577de0cd/raw/356ce2c39dfd58d3d2e948d1ad87ea828544f1db/accounts.json";
+  Response response = await get(Uri.parse(url));
+  return json.decode(response.body);
+}
+
+Future<void> sendDataAsync(Map<String, dynamic> mapAccount) async {
+  List<dynamic> listAccounts = await requestDataAsync();
+  listAccounts.add(mapAccount);
+  String content = json.encode(listAccounts);
+  print(content);
 }
